3327f8e880f819853cb59112127fd9cf8838df82
VickyGennadevna <vichka.z.2013@mail.ru>
2022-01-20 17:38:56 +0300

    task3

---
+++ task_3/task3.py
@@ -0,0 +1,9 @@
+user_number = input('Введите число n: ')
+
+number1 = user_number
+number2 = user_number * 2
+number3 = user_number * 3
+
+result = int(number1) + int(number2) + int(number3)
+
+print(result)
